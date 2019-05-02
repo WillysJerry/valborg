7e4b8d6146fe0d841092b235ba07d79a2dc820e2
Christian Mortensen <chrille@jinxed.se>
2019-05-02 15:49:46 +0200

    Add remaining parallel map functions Seem to work OK!

--- src/runtime/parallel/parallel.c
+++ src/runtime/parallel/parallel.c
@@ -25,6 +25,96 @@ void merge_result(dist_ret** ret, par_array* result) {
 	}
 }
 
+void map_thrd(distribution dist, int id, dist_ret* retval, void* f, void* p) {
+	int size = dist.b_size[id];
+	double* arr = (double*)calloc(dist.b_size[id], sizeof(double));
+
+	// Just assume that these arrays were passed...
+	double* A = dist.arrs[0] + dist.blocks[id];
+
+	// Cast void pointers to functions, this is not very readable
+	double (*func)(double x) =
+		(double (*)(double x)) f;
+	int (*pred)(int i) =
+		(int (*)(int)) p;
+
+
+	for(int i = 0; i < size; i++) {
+		if(pred == NULL || pred(i)) {
+			arr[i] = func(A[i]);
+		}
+	}	
+
+	retval->v = arr;
+	retval->n = size;
+}
+
+par_array par_map1(double (*f)(double x), const par_array a, int (*p)(int i)) {
+	par_array result;
+	par_array arrs[] = { a };
+	distribution dist = distribute(arrs, 1);
+
+	// Start parallel execution
+	dist_ret** ret = execute_in_parallel(map_thrd, dist, (void*)f, (void*)p);
+
+	result.a = NULL;	// If none of the input arrays index sets intersect, we'll return a NULL pointer 
+	result.m = dist.m;	
+	result.n = dist.m - 1;	// Initialize n to be one less then m, as n,m are both inclusive (m=0,n=0 equals an array consisting of one element
+
+	if(ret != NULL) {
+		merge_result(ret, &result);
+		free(ret);
+	}
+
+	return result;
+
+}
+
+void map2_thrd(distribution dist, int id, dist_ret* retval, void* f, void* p) {
+	int size = dist.b_size[id];
+	double* arr = (double*)calloc(dist.b_size[id], sizeof(double));
+
+	// Just assume that these arrays were passed...
+	double* A = dist.arrs[0] + dist.blocks[id];
+	double* B = dist.arrs[1] + dist.blocks[id];
+
+	// Cast void pointers to functions, this is not very readable
+	double (*func)(double x, double y) =
+		(double (*)(double x, double y)) f;
+	int (*pred)(int i) =
+		(int (*)(int)) p;
+
+
+	for(int i = 0; i < size; i++) {
+		if(pred == NULL || pred(i)) {
+			arr[i] = func(A[i], B[i]);
+		}
+	}	
+
+	retval->v = arr;
+	retval->n = size;
+}
+
+par_array par_map2(double (*f)(double x, double y), const par_array a, const par_array b, int (*p)(int i)) {
+	par_array result;
+	par_array arrs[] = { a, b };
+	distribution dist = distribute(arrs, 2);
+
+	// Start parallel execution
+	dist_ret** ret = execute_in_parallel(map2_thrd, dist, (void*)f, (void*)p);
+
+	result.a = NULL;	// If none of the input arrays index sets intersect, we'll return a NULL pointer 
+	result.m = dist.m;	
+	result.n = dist.m - 1;	// Initialize n to be one less then m, as n,m are both inclusive (m=0,n=0 equals an array consisting of one element
+
+	if(ret != NULL) {
+		merge_result(ret, &result);
+		free(ret);
+	}
+
+	return result;
+}
+
 void map3_thrd(distribution dist, int id, dist_ret* retval, void* f, void* p) {
 	int size = dist.b_size[id];
 	double* arr = (double*)calloc(dist.b_size[id], sizeof(double));
--- tests/map.c
+++ tests/map.c
@@ -18,6 +18,7 @@ double mul3(double x, double y, double z) {
 }
 
 int main(int argc, char** argv) {
+	par_array R; 	// Resulting array
 	
 	double arr1[] = { 	1.0, 	2.0, 	3.0, 	4.0,	5.0,	6.0 };
 	double arr2[] = { 	3.0, 	4.0, 	5.0, 	6.0, 	7.0, 	8.0 };
@@ -27,11 +28,27 @@ int main(int argc, char** argv) {
 	const par_array B = mk_array(arr2, 0, 5);
 	const par_array C = mk_array(arr3, 0, 5);
 
-	// A,B,C intersect at [3,5]
-	par_array R = par_map3(mul3, A, B, C, NULL);
-
+	R = par_map1(square, A, NULL);
+	printf("Square A:\n");
 	for(int i = 0; i < length(R); i++) {
 		printf("%f ", R.a[i]);
 	}
+	free(R.a);
+
+	printf("\nSum A B:\n");
+	R = par_map2(sum2, A, B, NULL);
+	for(int i = 0; i < length(R); i++) {
+		printf("%f ", R.a[i]);
+	}
+	free(R.a);
+
+	printf("\nMul A B C:\n");
+	R = par_map3(mul3, A, B, C, NULL);
+	for(int i = 0; i < length(R); i++) {
+		printf("%f ", R.a[i]);
+	}
+	free(R.a);
+
 	printf("\n");
+	return 0;
 }
